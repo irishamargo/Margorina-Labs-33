b23c53f263b015a1c9f9b2d0bd34cf2e5345481f
irishamargo <97751041+irishamargo@users.noreply.github.com>
2025-12-18 02:15:04 +0300

    Delete sort.py

--- sort.py
+++
@@ -1,8 +0,0 @@
-data = [5, -5, 3, -3, 10, -10, 1, -1, 0, 7, -7]
-
-if __name__ == '__main__':
-    result = sorted(data, key=abs, reverse=True)
-    print("Без lambda:", result)
-
-    result_with_lambda = sorted(data, key=lambda x: abs(x), reverse=True)
-    print("С lambda:", result_with_lambda)
